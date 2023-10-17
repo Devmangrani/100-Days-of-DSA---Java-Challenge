0390eec433e55e97efe53c2186826f7649fcd274
Devmangrani <devmangrani5@gmail.com>
2023-10-18 00:22:31 +0530

    Day 01

--- Day 01/Day01.java
+++ Day 01/Day01.java
@@ -1,7 +1,22 @@
 // package Day 01;
 //53. Maximum Subarray
 public class Day01 {
+    public static int maxSubArray(int[] nums) {
+        //Here I have Used kadane's Algorithm
+        //in this algo we make 0 to negative sum
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            currSum = currSum + nums[i];
+            if (currSum<0) {
+                currSum=0;
+            }
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
     public static void main(String[] args) {
-        System.out.println("Day1");
+        int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
     }
 }
