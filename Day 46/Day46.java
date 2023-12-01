0ea46717496347e9ea43ab81dd1000a339981865
Devmangrani <devmangrani5@gmail.com>
2023-12-01 09:48:42 +0530

    Day 46

---
+++ Day 46/Day46.java
@@ -0,0 +1,31 @@
+public class Day46 {
+    /** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+
+        while(start<=end){
+          int mid = start + (end-start)/2;
+          int guessNumber = guess(mid);
+
+          if(guessNumber == -1){
+            end = mid - 1;
+          }else if(guessNumber == 1){
+            start = mid + 1;
+          }else {
+            return mid;
+          }
+        }
+        return -1;
+    }
+}
+}
