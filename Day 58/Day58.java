f5eca803a4f178a940f68b68ab76dbaa4952968a
Devmangrani <devmangrani5@gmail.com>
2023-12-14 12:01:18 +0530

    Day 58 Edited

--- Day 58/Day58.java
+++ Day 58/Day58.java
@@ -1,8 +1,11 @@
-class Solution {
-    public int maxProduct(int[] nums) {
-        int max = nums.length-1;
-        Arrays.sort(nums);
-        int res = (nums[max]-1)*(nums[max-1]-1);
-        return res;
+import java.util.*;
+public class Day58 {
+    class Solution {
+        public int maxProduct(int[] nums) {
+            int max = nums.length-1;
+            Arrays.sort(nums);
+            int res = (nums[max]-1)*(nums[max-1]-1);
+            return res;
+        }
     }
 }
