19e652ab0d7dbb625a6153d26a6ca50959959c33
Devmangrani <devmangrani5@gmail.com>
2023-12-26 10:49:28 +0530

    Day 71

---
+++ Day 71/Day71.java
@@ -0,0 +1,13 @@
+public class Day71 {
+    class Solution {
+        public int maxWidthOfVerticalArea(int[][] points) {
+            Arrays.sort(points , (a,b) -> Integer.compare(a[0],b[0]));
+            int max = 0;
+            for(int i = 1; i < points.length ; i++){
+              int wide = points[i][0] - points[i-1][0];
+              max = Math.max(max, wide);
+            }
+            return max;
+        }
+    }
+}
